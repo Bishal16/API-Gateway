06c3e054bf439ea3e4ae17962d38c13ed16a6ce4
Momenuzzaman <mdeamon4@gmail.com>
2024-04-25 09:07:59 +0600

    cors issue resolveed

--- src/main/java/com/telcobright/gateway/config/CorsConfig.java
+++ src/main/java/com/telcobright/gateway/config/CorsConfig.java
@@ -7,25 +7,23 @@ import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 import org.springframework.web.cors.reactive.CorsWebFilter;
 
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 @Configuration
-public class GatewayConfig {
+public class CorsConfig {
 
     @Bean
     public CorsWebFilter corsFilter() {
         CorsConfiguration config = new CorsConfiguration();
 
-        // Allowed Origins
-        List<String> allowedOrigins = Arrays.asList("http://localhost:3000");
+        List<String> allowedOrigins = List.of("http://localhost:3000");
         config.setAllowedOrigins(allowedOrigins);
 
-        // Allowed Methods
+
         List<String> allowedMethods = Arrays.asList("GET", "POST", "PUT", "DELETE");
         config.setAllowedMethods(allowedMethods);
 
-        config.setMaxAge(3600L);
+
         config.addAllowedHeader("*");
         config.setAllowCredentials(true);
 
